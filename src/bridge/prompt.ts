3c4e1b0e84792f3efab090652eec0154700fa38b
Zhao Yunlong <zao44457660@126.com>
2023-06-29 18:53:56 +0100

    init prompt

---
+++ src/bridge/prompt.ts
@@ -0,0 +1,17 @@
+
+export const PROMPT = {
+    PICK: function(player: String, item: String, 
+        Surroundings: String| null, word_cnt: number = 20){
+        if(Surroundings===null)
+            return `polish this sentence to make it more story, no more than ${word_cnt} words: The ${player} picks ${item}`
+        else
+            return `polish this sentence to make it more story, no more than ${word_cnt} words: The ${player} picks ${item} in/on/at ${Surroundings}`
+    },
+    BEAT: function(sponsor: String, target: String, 
+        result: String, word_cnt = 20){
+        return `polish this sentence to make it more story, no more than ${word_cnt} words: ${sponsor} stroke ${target}, and the result is ${result}`
+    },
+    RIDDLE: function(number: number){
+        return `create a riddle whose answer is ${number}`
+    }
+}
