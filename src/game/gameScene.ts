d74de32003726df69ff15cdd6c9fe9f86a59d591
PneuC <50522987+PneuC@users.noreply.github.com>
2023-06-30 02:03:06 +0800

    add item
    Add item

--- src/game/gameScene.ts
+++ src/game/gameScene.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 import tile from "../../public/images/Tilemap/tilemap_packed.png"
 import playerimg from "../../public/images/Tiles/tile_0085.png"
+import itemimg from "../../public/images/Tiles/tile_0072.png"
+import Item from "./item"
 import Player from "./player";
 
 export default class MainScene extends Phaser.Scene{
@@ -11,8 +13,10 @@ export default class MainScene extends Phaser.Scene{
     preload(){
         this.load.image('background', tile)
         this.load.image('player', playerimg)
+        this.load.image('item', itemimg)
     }
     player
+    item
     create(){
         this.add.image(0, 0, 'background').setOrigin(0, 0)
         const array =[[0,1,3],[0,1,2],[0,1,2]]
@@ -22,9 +26,22 @@ export default class MainScene extends Phaser.Scene{
 
         this.player = new Player(this, 400, 300)
         this.add.existing(this.player)
+
+        this.item = new Item(this, 500, 400, "Box")
+        this.add.existing(this.item)
     }
 
     update(time: number, delta: number): void {
-        this.player.update(delta)
+      this.player.update(delta)
+      this.collision_detect()
+    }
+
+    collision_detect() {
+      let dx = this.player.x - this.item.x
+      let dy = this.player.y - this.item.y
+      if (dx * dx + dy * dy < 100 && !this.item.collected) {
+        console.log('Collision')
+        this.item.on_collected()
+      }
     }
 }
