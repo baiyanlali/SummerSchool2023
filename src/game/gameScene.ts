03eb02ef482cc9ef50ce4d9d4b71b304f12d86c8
Zhao Yunlong <zao44457660@126.com>
2023-06-29 09:35:09 +0100

    update

--- src/game/gameScene.ts
+++ src/game/gameScene.ts
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import tile from "../../public/images/Tilemap/tilemap_packed.png"
 
 export default class MainScene extends Phaser.Scene{
     constructor(){
@@ -6,10 +7,15 @@ export default class MainScene extends Phaser.Scene{
     }
 
     preload(){
-
+        this.load.image('background', tile)
     }
 
     create(){
-        
+        this.add.image(0, 0, 'background').setOrigin(0, 0)
+        console.log("hello")
+        const array =[[0,1,3],[0,1,2],[0,1,2]]
+        const map = this.make.tilemap({data: array, tileWidth: 16, tileHeight: 16})
+        map.addTilesetImage("tilemap", "tiles", 16, 16)
+        const layer = map.createLayer(0, "tilemap", 0, 0)
     }
 }
