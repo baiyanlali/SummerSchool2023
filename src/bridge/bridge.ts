6079687f5934a6995f700b113ea66d910117e356
Zhao Yunlong <zao44457660@126.com>
2023-06-29 19:15:52 +0100

    update

--- src/bridge/bridge.ts
+++ src/bridge/bridge.ts
@@ -4,13 +4,17 @@ class Bridge{
     chat: webllm.ChatModule
     messages: Array<String> = []
     results: Array<String> = []
+    onGenFin = (r)=>{}
     constructor(){
         this.chat = new webllm.ChatModule();
     }
 
     generating = false
 
-    async init(){
+    async init(onProgress){
+        this.chat.setInitProgressCallback((report: webllm.InitProgressReport) => {
+            onProgress(report.text)
+        });
         await this.chat.reload("vicuna-v1-7b-q4f32_0")
     }
 
@@ -19,11 +23,13 @@ class Bridge{
             let message = this.messages.pop()
             if(message === undefined)return
             let result = await this.chat.generate(message)
+            this.onGenFin(result)
             this.results.push(result)
         }
     }
 
-    async sendMessage(message: String){
+    sendMessage(message: String){
+        console.log(`rec: ${message}`)
         this.messages.push(message)
         if(!this.generating){
             this.gen()
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,6 +1,14 @@
 import Phaser from "phaser";
 import MainScene from "./gameScene";
 
+function setLabel(id: string, text: string) {
+    const label = document.getElementById(id);
+    if (label == null) {
+      throw Error("Cannot find label " + id);
+    }
+    label.innerText = text;
+  }
+
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.CANVAS,
     width: 800,
@@ -10,4 +18,11 @@ const config: Phaser.Types.Core.GameConfig = {
 }
 
 const GAME = new Phaser.Game(config)
+
 // GAME.scale.displaySize = 5
+import { bridge } from "../bridge/bridge";
+
+bridge.init((p)=>{setLabel("init-label", p);}).then(()=>{bridge.sendMessage("This is a game")})
+bridge.onGenFin = (r)=>{
+    console.log(r)
+}
