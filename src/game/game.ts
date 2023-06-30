4d9b9148fd6791aea7c4b4dc6f75cd21e187adcb
Zhao Yunlong <zao44457660@126.com>
2023-06-30 11:57:27 +0100

    修改位置布局

--- src/game/game.ts
+++ src/game/game.ts
@@ -16,7 +16,8 @@ const config: Phaser.Types.Core.GameConfig = {
     width: 800,
     height: 600,
     scene: [gameMainScene],
-    backgroundColor: "#1FFFFF"
+    backgroundColor: "#1FFFFF",
+    parent: "game"
 }
 
 const GAME = new Phaser.Game(config)
@@ -25,10 +26,13 @@ const GAME = new Phaser.Game(config)
 import { bridge } from "../bridge/bridge";
 
 if(true){
+    const stories: Array<String> = []
     bridge.init((p)=>{setLabel("init-label", p);}).then(()=>{bridge.
         sendMessage("This is a game where player vincent collect what he lost, please polish every sentence you have met to make like a story. Answer this sentence with no more than 5 words.")})
     bridge.onGenFin = (r)=>{
         gameMainScene.aside.text = r
+        stories.push(r)
+        setLabel("generate-label", stories.join("<br>"))
         // console.log(r)
     }
     bridge.onProgress = (s, m)=>{
