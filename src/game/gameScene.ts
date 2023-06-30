b9e9cd8fa51619086e6553c79ee12df9cf0bd111
Zhao Yunlong <zao44457660@126.com>
2023-06-30 11:05:42 +0100

    Update gameScene.ts

--- src/game/gameScene.ts
+++ src/game/gameScene.ts
@@ -25,11 +25,11 @@ export default class MainScene extends Phaser.Scene{
     items: Item[]
     aside: Phaser.GameObjects.Text
     create(){
-        this.add.image(0, 0, 'background').setOrigin(0, 0)
-        const array =[[0,1,3],[0,1,2],[0,1,2]]
-        const map = this.make.tilemap({data: array, tileWidth: 16, tileHeight: 16})
-        map.addTilesetImage("background", "background", 16, 16)
-        const layer = map.createLayer(0, "background", 0, 0)
+        // this.add.image(0, 0, 'background').setOrigin(0, 0)
+        // const array =[[0,1,3],[0,1,2],[0,1,2]]
+        // const map = this.make.tilemap({data: array, tileWidth: 16, tileHeight: 16})
+        // map.addTilesetImage("background", "background", 16, 16)
+        // const layer = map.createLayer(0, "background", 0, 0)
 
         this.player = new Player(this, 400, 300)
         this.add.existing(this.player)
